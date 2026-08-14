e90f028e22402b5008ea0912873ed1077100c5ea
rogelio <rogelio@gpr.design>
2026-08-14 16:29:17 -0300

    feat: aplica black-branding e compila ultima versao

--- Customizar_UI.bat
+++ Customizar_UI.bat
@@ -1,3 +1,2 @@
 @echo off
-rem Abre o programa salvando as configurações direto na pasta do projeto
-start "" "%~dp0bin\FreeCAD.exe" -u "%~dp0config\user.cfg" -s "%~dp0config\system.cfg"
+start bin\CoreCAD.exe
